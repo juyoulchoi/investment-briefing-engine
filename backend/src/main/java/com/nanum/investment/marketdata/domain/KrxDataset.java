f97b773555240d984da98680a1d290554dcb8bee
juyoulchoi <juyoulchoi@nate.com>
2026-08-26 00:11:26 +0900

    refactor: map KRX datasets to common market tables

--- backend/src/main/java/com/nanum/investment/marketdata/domain/KrxDataset.java
+++ backend/src/main/java/com/nanum/investment/marketdata/domain/KrxDataset.java
@@ -6,11 +6,17 @@ import java.util.List;
 public enum KrxDataset {
   KOSPI_STOCK_MASTER("/svc/apis/sto/stk_isu_base_info", List.of("ISU_CD", "ISU_SRT_CD")),
   KOSDAQ_STOCK_MASTER("/svc/apis/sto/ksq_isu_base_info", List.of("ISU_CD", "ISU_SRT_CD")),
+  ALL_STOCK_MASTER("/svc/apis/sto/isu_base_info", List.of("ISU_CD", "ISU_SRT_CD")),
+  KONEX_STOCK_MASTER("/svc/apis/sto/knx_isu_base_info", List.of("ISU_CD", "ISU_SRT_CD")),
   KOSPI_STOCK_DAILY("/svc/apis/sto/stk_bydd_trd", List.of("ISU_CD", "ISU_SRT_CD")),
   KOSDAQ_STOCK_DAILY("/svc/apis/sto/ksq_bydd_trd", List.of("ISU_CD", "ISU_SRT_CD")),
+  KONEX_STOCK_DAILY("/svc/apis/sto/knx_bydd_trd", List.of("ISU_CD", "ISU_SRT_CD")),
+  SUBSCRIPTION_WARRANT_DAILY("/svc/apis/sto/sw_bydd_trd", List.of("ISU_CD", "ISU_SRT_CD")),
+  SUBSCRIPTION_RIGHT_DAILY("/svc/apis/sto/sr_bydd_trd", List.of("ISU_CD", "ISU_SRT_CD")),
   KOSPI_INDEX_DAILY("/svc/apis/idx/kospi_dd_trd", List.of("IDX_CLSS", "IDX_NM")),
   KOSDAQ_INDEX_DAILY("/svc/apis/idx/kosdaq_dd_trd", List.of("IDX_CLSS", "IDX_NM")),
   KRX_INDEX_DAILY("/svc/apis/idx/krx_dd_trd", List.of("IDX_CLSS", "IDX_NM")),
+  BOND_INDEX_DAILY("/svc/apis/idx/bon_dd_trd", List.of("IDX_CLSS", "IDX_NM")),
   DERIVATIVE_INDEX_DAILY("/svc/apis/idx/drvprod_dd_trd", List.of("IDX_CLSS", "IDX_NM")),
   FUTURES_DAILY("/svc/apis/drv/fut_bydd_trd", List.of("ISU_CD", "ISU_SRT_CD")),
   ETF_DAILY("/svc/apis/etp/etf_bydd_trd", List.of("ISU_CD", "ISU_SRT_CD"));
--- backend/src/test/java/com/nanum/investment/marketdata/application/KrxDatasetTest.java
+++ backend/src/test/java/com/nanum/investment/marketdata/application/KrxDatasetTest.java
@@ -13,6 +13,12 @@ class KrxDatasetTest {
     assertEquals(KrxDataset.KOSPI_STOCK_DAILY, KrxDataset.fromApiMethod("stk_bydd_trd"));
     assertEquals(KrxDataset.DERIVATIVE_INDEX_DAILY, KrxDataset.fromApiMethod("drvprod_dd_trd"));
     assertEquals(KrxDataset.FUTURES_DAILY, KrxDataset.fromApiMethod("fut_bydd_trd"));
+    assertEquals(KrxDataset.BOND_INDEX_DAILY, KrxDataset.fromApiMethod("bon_dd_trd"));
+    assertEquals(KrxDataset.KONEX_STOCK_DAILY, KrxDataset.fromApiMethod("knx_bydd_trd"));
+    assertEquals(KrxDataset.SUBSCRIPTION_WARRANT_DAILY, KrxDataset.fromApiMethod("sw_bydd_trd"));
+    assertEquals(KrxDataset.SUBSCRIPTION_RIGHT_DAILY, KrxDataset.fromApiMethod("sr_bydd_trd"));
+    assertEquals(KrxDataset.ALL_STOCK_MASTER, KrxDataset.fromApiMethod("isu_base_info"));
+    assertEquals(KrxDataset.KONEX_STOCK_MASTER, KrxDataset.fromApiMethod("knx_isu_base_info"));
   }
 
   @Test
